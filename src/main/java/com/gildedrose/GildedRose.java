29512affb50af1986fa49a23df918a6ed3e75d8d
honda-yusuke <honda-yusuke@dmm.com>
2020-10-11 16:06:06 +0900

    BackstagePassesにおいてSellIn値が0未満になったさいのQuality値更新処理をリファクタリング

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,7 +48,7 @@ class GildedRose {
                 }
 
                 if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
+                    item.quality = 0;
                 }
             }
         } else {
