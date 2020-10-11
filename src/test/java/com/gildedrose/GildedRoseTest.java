ef517b14cc3c2fa69eb346ff49d615675e28c39c
honda-yusuke <honda-yusuke@dmm.com>
2020-10-11 15:30:55 +0900

    BackstagePassesに関するテストケース追加

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -114,4 +114,81 @@ class GildedRoseTest {
         }
     }
 
+    @Nested
+    class _BackstagePassesの場合 {
+        @BeforeEach
+        void _準備() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30) };
+        }
+
+        @Test
+        void _Item名がBackstagePassesの場合更新してもItem名が変わらない() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        }
+
+        @Test
+        void _Item名がBackstagePassesの場合更新してもSellIn値が1減る() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(19, app.items[0].sellIn);
+        }
+
+        @Test
+        void _Item名がBackstagePassesの場合更新してもQuality値が1増える() {
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(31, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでQuality値が49の場合更新してもQuality値が1増える() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20, 49) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(50, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでQuality値が50の場合更新してもQuality値が変わらない() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20, 49) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(50, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでSellIn値が11の場合更新してもQuality値は1増える() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(31, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでSellIn値が10の場合更新してもQuality値は2増える() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(32, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでSellIn値が6の場合更新してもQuality値は2増える() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 6, 30) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(32, app.items[0].quality);
+        }
+
+        @Test
+        void _Item名がBackstagePassesでSellIn値が5の場合更新してもQuality値は3増える() {
+            items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals(33, app.items[0].quality);
+        }
+    }
+
 }
