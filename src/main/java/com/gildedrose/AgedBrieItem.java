571f394677f870250d37b8a93e9a85cf2b194d76
honda-yusuke <honda-yusuke@dmm.com>
2020-10-11 16:30:40 +0900

    AgedBrieItemクラスを追加

---
+++ src/main/java/com/gildedrose/AgedBrieItem.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+public class AgedBrieItem implements GildedRoseItem {
+    @Override
+    public void updateSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    @Override
+    public void updateQuality(Item item) {
+        if (item.quality < 50) {
+            incrementQuality(item);
+        }
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                incrementQuality(item);
+            }
+        }
+    }
+
+    private void incrementQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+}
