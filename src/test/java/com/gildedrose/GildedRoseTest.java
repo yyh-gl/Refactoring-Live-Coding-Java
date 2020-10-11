8e0443a5ddcd7d5c7b13a58313a1e23a00941bd5
honda-yusuke <honda-yusuke@dmm.com>
2020-10-11 14:58:14 +0900

    テストケース追加

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,40 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    Item[] items;
+    
+    @BeforeEach
+    void _準備() {
+         items = new Item[] { new Item("foo", 20, 30) };
+    }
+    
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void _Item名の更新() {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void _sellIn値の更新() {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19, app.items[0].sellIn);
+    }
+
+    @Test
+    void _quality値の更新() {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(29, app.items[0].quality);
     }
 
 }
