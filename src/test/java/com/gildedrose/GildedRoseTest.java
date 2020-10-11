efd4b072a95b1762d26652be68f73305432b123a
honda-yusuke <honda-yusuke@dmm.com>
2020-10-11 15:02:15 +0900

    sulfurasに関するテストケース追加

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -17,24 +17,48 @@ class GildedRoseTest {
     
 
     @Test
-    void _Item名の更新() {
+    void _更新してもItem名が変わらない() {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
     @Test
-    void _sellIn値の更新() {
+    void 更新すると_sellIn値が1減る() {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(19, app.items[0].sellIn);
     }
 
     @Test
-    void _quality値の更新() {
+    void 更新すると_quality値の更新が1減る() {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(29, app.items[0].quality);
+    }
+    
+    @Test
+    void _Item名が_Sulfurasの場合更新してもItem名が変わらない() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 20, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+    }
+
+    @Test
+    void _Item名が_Sulfurasの場合更新してもSellIn値が変わらない() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 20, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(20, app.items[0].sellIn);
+    }
+
+    @Test
+    void _Item名が_Sulfurasの場合更新してもQuality値が変わらない() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 20, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(30, app.items[0].quality);
     }
 
 }
